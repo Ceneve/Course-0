c37d7122356aaeff8ade2fb3fefc8944b35ae734
Boris Kozlow <boriskozlow@gmail.com>
2024-09-24 15:58:39 +0300

    feat: add displaying pictures on the page

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,4 +1,4 @@
-let url = "https://api.unsplash.com/search/photos?query=dog&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
+
 
 // function getData() {
 //     fetch(url)
@@ -6,17 +6,24 @@ let url = "https://api.unsplash.com/search/photos?query=dog&per_page=30&orientat
 //         return result.json();
 //       });
 // };
+// console.log(a[0].urls.small);
 
-function getData() {
+
+const content = document.querySelector(".content");
+
+let defaultUrl = "https://api.unsplash.com/search/photos?query=dog&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
+
+function imageGenerator(url) {
     fetch(url)
     .then(result => result.json())
     .then(data => {
-        let a = data.results;
-        return console.log(a[0].urls.small);
+        let dataArray = data.results;
+        for (i=0;  i<30; i++ ) {
+            let image = document.createElement('img');
+            image.src = dataArray[i].urls.small;
+            content.appendChild(image);
+        }
     })
-
 };
 
-
-
-getData();
+imageGenerator(defaultUrl);
