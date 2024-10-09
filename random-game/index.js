20a91d5bf9f1d0b92ab6c84653f8186c5030e91e
Boris Kozlow <boriskozlow@gmail.com>
2024-10-09 23:03:42 +0300

    feat: add score, lives, start, end buttons

--- random-game/index.js
+++ random-game/index.js
@@ -1,10 +1,17 @@
 const mainScreen = document.querySelector('.main-screen');
+let score = document.getElementById('score');
+let lives = document.getElementById('lives');
+const stopButton = document.getElementById('stop-button');
+const startButton = document.getElementById('start-button');
 const balloonCount = 10;
 const balloonInterval = 1000;
+let balloonDestroyed = 0;
+let liveCount = 10;
 
 let currentBalloons = 0;
 
 function createBalloon() {
+  let audioUrl = 'assets/audio/Balloon1.mp3'
   const balloon = document.createElement('img');
   balloon.src = 'assets/img/balloon.png';
   balloon.style.position = 'absolute';
@@ -16,21 +23,31 @@ function createBalloon() {
  
   balloon.addEventListener('click', () => {
     balloon.remove();
+    playAudio(audioUrl);
     currentBalloons--;
+    balloonDestroyed++;
   });
 
   mainScreen.appendChild(balloon);
+  score.innerText = balloonDestroyed * 100;
   currentBalloons++;
 
 
   let top = parseInt(balloon.style.top);
-  let speed = 3;
+  let speed = 2;
 
   function animate() {
     top -= speed;
     balloon.style.top = `${top}px`;
 
-    if (top < 0) {
+    if (top === 0) {
+      liveCount--;
+      lives.innerText = liveCount;
+      if (liveCount === 0) {
+        stopCreateBalloons();
+      }
+    }
+    if (top <= 0) {
       top = 500;
     }
 
@@ -39,14 +56,31 @@ function createBalloon() {
 
   animate();
 
-  currentBalloons++;
+  // currentBalloons++;
 }
 
 function createBalloons() {
   if (currentBalloons < balloonCount) {
+    timeout = setTimeout(createBalloons, balloonInterval);
     createBalloon();
-    setTimeout(createBalloons, balloonInterval);
+    
   }
 }
 
-createBalloons();
+// createBalloons();
+
+function stopCreateBalloons() {
+  clearTimeout(timeout);
+  score.innerText = '0';
+  mainScreen.innerHTML = '';
+  currentBalloons = 0;
+  balloonDestroyed = 0;
+}
+
+
+startButton.addEventListener('click', createBalloons);
+stopButton.addEventListener('click', stopCreateBalloons);
+
+function playAudio(url) {
+  new Audio(url).play();
+}
